3357bee6e01de04f9a4530fe7d4a1fed14a52885
ethanpark0726 <43104410+ethanpark0726@users.noreply.github.com>
2021-09-14 14:47:27 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,6 +1,6 @@
 prod_server = 'https://gm.ib.hsnet.ufl.edu/wapi/v2.10.5/'
-prod_server_id = 'ibnetengapi'
-prod_server_pw = 'n3tap1m@g1c'
+prod_server_id = 'yourID'
+prod_server_pw = 'yourPW'
 headers = {'Content-type': 'application/json'}
 auth = (prod_server_id, prod_server_pw)
 group = 'INFOBLOX_CPC'
