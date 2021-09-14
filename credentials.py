4fbaf91ac798af9375238f00f4ad227178659375
ethanpark0726 <43104410+ethanpark0726@users.noreply.github.com>
2021-09-14 14:48:28 -0400

    Delete credentials.py

--- credentials.py
+++
@@ -1,6 +0,0 @@
-prod_server = 'yourServer'
-prod_server_id = 'yourID'
-prod_server_pw = 'yourPW'
-headers = {'Content-type': 'application/json'}
-auth = (prod_server_id, prod_server_pw)
-group = 'INFOBLOX_CPC'
