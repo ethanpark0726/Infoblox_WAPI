c3104b472a604721397303f46dd2f59645a2af0e
Park <hpar0001@shands.ufl.edu>
2021-09-14 14:43:30 -0400

    First commit

---
+++ credentials.py
@@ -0,0 +1,6 @@
+prod_server = 'https://gm.ib.hsnet.ufl.edu/wapi/v2.10.5/'
+prod_server_id = 'ibnetengapi'
+prod_server_pw = 'n3tap1m@g1c'
+headers = {'Content-type': 'application/json'}
+auth = (prod_server_id, prod_server_pw)
+group = 'INFOBLOX_CPC'
