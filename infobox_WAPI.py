cf8661e0aa2b04cf72c5d05f415cc8c99a9403a4
ethanpark0726 <43104410+ethanpark0726@users.noreply.github.com>
2021-09-14 14:53:11 -0400

    Update infobox_WAPI.py

--- infobox_WAPI.py
+++ infobox_WAPI.py
@@ -4,7 +4,13 @@ import urllib3
 import json
 import pprint
 from cfonts import render
-import credentials
+
+prod_server = 'yourServerIP'
+prod_server_id = 'yourID'
+prod_server_pw = 'yourPW'
+headers = {'Content-type': 'application/json'}
+auth = (prod_server_id, prod_server_pw)
+group = 'INFOBLOX_CPC'
 
 auth = (prod_server_id, prod_server_pw)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
